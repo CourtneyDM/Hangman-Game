c75f27e59e090406c43bdd047343bbc3a2ab7ca1
Courtney Montgomery <courtdmontgomery@gmail.com>
2018-02-11 13:53:26 -0600

    added content to index.html, created variables in game.js and updated tasks in README.md

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # Hangman-Game
 Javascript version of Hangman game
 
+
+
+
 ## Tasks
-- [ ] Create a variable to display the number of guesses
-- [ ] Create a variable to display the letter guessed
-- [ ] Create a variable to display the total number of wins
-- [ ] Create an array to store the letters guessed
-- [ ] Create an array to store random words to be used for game
+- [X] Create a variable to display the number of guesses
+- [X] Create a variable to display the letter guessed
+- [X] Create a variable to display the total number of wins
+- [X] Create an array to store the letters guessed
+- [X] Create an array to store random words to be used for game
---
+++ assets/javascript/game.js
@@ -0,0 +1,49 @@
+// Variable Declarations
+var numOfGuesses;
+var letter;
+var letterGuessed = [];
+var numOfWins;
+
+var superhero = [
+    {
+        alias: "Arthur Curry",
+        superhero: "Aquaman"
+    },
+    {
+        alias: "Bruce Wayne",
+        superhero: "Batman"
+    },
+    {
+        alias: "Steven Rogers",
+        superhero: "Captain America"
+    },
+    {
+        alias: "Oliver Queen",
+        superhero: "Green Arrow"
+    },
+    {
+        alias: "Bruce Banner",
+        superhero: "Incredible Hulk"
+    },
+    {
+        alias: "Tony Stark",
+        superhero: "Ironman"
+    },
+    {
+        alias: "Anna Marie",
+        superhero: "Rogue"
+
+    },
+    {
+        alias: "Peter Parker",
+        superhero: "Spider-man"
+    },
+    {
+        alias: "Clark Kent",
+        superhero: "Superman"
+    },
+    {
+        alias: "Princes Diana",
+        superhero: "Wonder Woman"
+    }
+];
