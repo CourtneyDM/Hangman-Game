2e2a36818f708aba7704fd6f9727e1204e7f0397
Courtney Montgomery <courtdmontgomery@gmail.com>
2018-02-16 15:46:56 -0600

    updated background image, created modal for displaying superhero and instructions, started styling in CSS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,122 +1,169 @@
 // Global Variable Declarations
-var index;          // Used as index for superheroes Object
-var letter;         // Stores value of key pressed
-var guesses = [];   // Stores all keys pressed
-var lives = 10;     // Number of guesses allowed
+var index; // Used as index for superheroes Object
+var letter; // Stores value of key pressed
+var guesses = []; // Stores all keys pressed
+var lives = 10; // Number of guesses allowed
 var nameArray = []; // Stores the names
-var numOfWins = 0;  // Store number of wins
+var numOfWins = 0; // Store number of wins
 
 // HTML Modifiers
 var button = document.getElementById("restart");
-var current = document.getElementById("current");               // Show current letter guessed
-var guessed = document.getElementById("guessed");               // Show letters guessed
-var identity = document.getElementById("identity");             // Hide/reveal alias
-var showLives = document.getElementById("showLives");            // Show lives left
-var showWins = document.getElementById("showWins");             // Show total wins
-var superhero = document.getElementById("superhero");
-var superhero_img = document.getElementById("superhero_img");   // Insert image after win
+var current = document.getElementById("current"); // Show current letter guessed
+var guessed = document.getElementById("guessed"); // Show letters guessed
+var identity = document.getElementById("identity"); // Hide/reveal alias
+var showLives = document.getElementById("showLives"); // Show lives left
+var showWins = document.getElementById("showWins"); // Show total wins
+var superhero = document.getElementById("superhero"); // Show superhero name
+var superhero_img = document.getElementById("superhero_img"); // Insert image after win
+var tagline = document.getElementById("tagline"); // Show hint associated with superhero
+
+// HTML Buttons
+var exitBtn = document.getElementById("exitBtn");
+var restartBtn = document.getElementById("restartBtn");
+var revealBtn = document.getElementById("revealBtn");
+
+
 
 // Superhero Object
-var superheroes = [
-    {
+var superheroes = [{
         alias: "Arthur Curry",
         superhero: "Aquaman",
-        image: "assets/images/aquaman.jpg"
+        publisher: "DC Comics",
+        tagline: "Dressed like a bat. I dig it.",
+        image: "assets/images/superheroes/aquaman.jpg"
     },
     {
         alias: "Natalia Allanovna Romanova",
         superhero: "Black Widow",
-        image: "assets/images/black-widow.jpg"
+        publisher: "Marvel Comics",
+        tagline: "Cognitive Re-Calibration: I hit you really hard in the head.",
+        image: "assets/images/superheroes/black-widow.jpg"
     },
     {
         alias: "Bruce Wayne",
         superhero: "Batman",
-        image: "assets/images/batman.jpg"
+        publisher: "DC Comics",
+        tagline: "I am vengeance, I am the night. I am...",
+        image: "assets/images/superheroes/batman.jpg"
     },
     {
         alias: "Steven Rogers",
         superhero: "Captain America",
-        image: "assets/images/captain-america.jpg"
+        publisher: "Marvel Comics",
+        tagline: "Not a perfect soldier, but a good man.",
+        image: "assets/images/superheroes/captain-america.jpg"
     },
     {
         alias: "Victor Stone",
         superhero: "Cyborg",
-        image: "assets/images/cyborg.jpg"
+        publisher: "DC Comics",
+        tagline: "I heard about you. Didn't think you were real.",
+        image: "assets/images/superheroes/cyborg.jpg"
     },
     {
         alias: "Wade Wilson",
         superhero: "Deadpool",
-        image: "assets/images/deadpool.jpg"
+        publisher: "Marvel Comics",
+        tagline: "Hey you. Yeah You. No, not you...the other guy. You right there! Yes, you! ...Do you like tacos?",
+        image: "assets/images/superheroes/deadpool.jpg"
     },
     {
         alias: "Barry Allen",
         superhero: "The Flash",
-        image: "assets/images/the-flash.jpg"
+        publisher: "DC Comics",
+        tagline: "Life doesn't give us purpose, we give life purpose.",
+        image: "assets/images/superheroes/the-flash.jpg"
     },
     {
         alias: "Oliver Queen",
         superhero: "Green Arrow",
-        image: "assets/images/green-arrow.jpg"
+        publisher: "DC Comics",
+        tagline: "An arrow can only be shot by pulling backward...",
+        image: "assets/images/superheroes/green-arrow.jpg"
     },
     {
         alias: "Bruce Banner",
         superhero: "Incredible Hulk",
-        image: "assets/images/incredible-hulk.jpg"
+        publisher: "Marvel Comics",
+        tagline: "Don't make me angry. You wouldn't like me when I'm angry.",
+        image: "assets/images/superheroes/incredible-hulk.jpg"
     },
     {
         alias: "Tony Stark",
         superhero: "Iron Man",
-        image: "assets/images/iron-man.jpg"
+        publisher: "Marvel Comics",
+        tagline: "I told you. I don't want to join your super-secret boy band.",
+        image: "assets/images/superheroes/iron-man.jpg"
     },
     {
         alias: "Magnus Eisenhardt",
         superhero: "Magneto",
-        image: "assets/images/magneto.jpg"
+        publisher: "Marvel Comics",
+        tagline: "Are you a God-fearing man, Senator?",
+        image: "assets/images/superheroes/magneto.jpg"
     },
     {
         alias: "Raven Darkholme",
         superhero: "Mystique",
-        image: "assets/images/mystique.png"
+        publisher: "Marvel Comics",
+        tagline: "...and I'm way more mysterious than you.",
+        image: "assets/images/superheroes/mystique.jpg"
     },
     {
         alias: "Jean Grey",
         superhero: "Phoenix",
-        image: "assets/images/phoenix.jpg"
+        publisher: "Marvel Comics",
+        tagline: "I am fire and life incarnate! Now and forever - I am...",
+        image: "assets/images/superheroes/phoenix.jpg"
     },
     {
         alias: "Charles Francis Xavier",
         superhero: "Professor X",
-        image: "assets/images/professorX.jpg"
+        publisher: "Marvel Comics",
+        tagline: "Mutation: it is the key to our evolution. It has enabled us to evolve from a single-celled organism into the dominant species on the planet. This process is slow, and normally taking thousands and thousands of years. But every few hundred millennia, evolution leaps forward.",
+        image: "assets/images/superheroes/professorX.jpg"
     },
     {
         alias: "Anna Marie",
         superhero: "Rogue",
-        image: "assets/images/rogue.jpg"
+        publisher: "Marvel Comics",
+        tagline: "The first boy I ever kissed ended up in a coma for three weeks.",
+        image: "assets/images/superheroes/rogue.jpg"
     },
     {
         alias: "Peter Parker",
         superhero: "Spider-man",
-        image: "assets/images/spider-man.jpg"
+        publisher: "Marvel Comics",
+        tagline: "My spidey-senses are going crazy!",
+        image: "assets/images/superheroes/spider-man.jpg"
     },
     {
         alias: "Ororo Monroe",
         superhero: "Storm",
-        image: "assets/images/storm.jpg"
+        publisher: "Marvel Comics",
+        tagline: "By the Goddess!",
+        image: "assets/images/superheroes/storm.jpg"
     },
     {
         alias: "Clark Kent",
         superhero: "Superman",
-        image: "assets/images/superman.jpg"
+        publisher: "DC Comics",
+        tagline: "Up,up and away!",
+        image: "assets/images/superheroes/superman.jpg"
     },
     {
         alias: "James Logan Howlett",
         superhero: "Wolverine",
-        image: "assets/images/wolverine.jpg"
+        publisher: "Marvel Comics",
+        tagline: "I'm the best at what I do.",
+        image: "assets/images/superheroes/wolverine.jpg"
     },
     {
         alias: "Princess Diana of Themyscira",
         superhero: "Wonder Woman",
-        image: "assets/images/wonder-woman.jpg"
+        publisher: "DC Comics",
+        tagline: "I will fight for those that cannot fight for themselves.",
+        image: "assets/images/superheroes/wonder-woman.jpg"
     }
 ];
 
@@ -125,28 +172,44 @@ var superheroes = [
 document.onload = startGame(numOfWins);
 
 // Capture letter on key up
-document.onkeyup = function (event) {
+document.onkeyup = function(event) {
     // console.log(event.keyCode);
     if (event.keyCode > 122 ||
         (event.keyCode < 97 && event.keyCode > 90) ||
         (event.keyCode < 65 && event.keyCode > 75) ||
         (event.keyCode < 48)) {
         letter = "";
-    }
-    else {
+    } else {
         letter = event.key.toLowerCase();
         current.innerText = letter;
         guessedLetter(letter);
     }
 }
 
+exitBtn.onclick = function() {
+    window.close();
+}
+
+revealBtn.onclick = function() {
+    identity.innerHTML = superheroes[index].alias.toLowerCase();
+    $("#stage").modal("show");
+};
+
+
+// Start of Game
 function startGame(wins) {
+    var show = "hello";
+    $('#myModal').modal('show');
     index = Math.floor(Math.random() * superheroes.length); // Random number used to select random alias
-    hideName(superheroes[index].alias.toLowerCase());       // Will display dashes for each character in alias selected
-    showWins.innerText = numOfWins;                         // Will display number of wins
-    showLives.innerText = lives;                            // Will display number of lives left
+    hideName(superheroes[index].alias.toLowerCase()); // Will display dashes for each character in alias selected
+    showWins.innerText = numOfWins; // Will display number of wins
+    showLives.innerText = lives; // Will display number of lives left
     wins = numOfWins;
 }
+
+restartBtn.onclick = function() {
+    location.reload()
+};
 
 // Display dashes on the screen
 function hideName(realName) {
@@ -154,10 +217,10 @@ function hideName(realName) {
     for (var i = 0; i < realName.length; i++) {
         if (realName[i] === " ") {
             nameArray.push(" ");
-        }
-        else nameArray.push("-");
+        } else nameArray.push("-");
     }
     identity.innerText = nameArray.join("");
+    tagline.innerText = '\"' + superheroes[index].tagline + '\"';
     return nameArray;
 }
 
@@ -174,11 +237,10 @@ function guessedLetter(newLetter) {
 
 // Look for the current letter in the alias
 function showName(letter) {
-    var superheroName = superheroes[index].superhero;           // Shorthand reference to superhero name
-    var img = document.createElement("img");                // Create HTML element to display image
-    var imgSrc = superheroes[index].image;                  // Use alias attribute to get superhero image
-    var name = superheroes[index].alias.toLowerCase();      // Shorthand reference to superhero's alias
-
+    var superheroName = superheroes[index].superhero; // Shorthand reference to superhero name
+    var img = document.createElement("img"); // Create HTML element to display image
+    var imgSrc = superheroes[index].image; // Use alias attribute to get superhero image
+    var name = superheroes[index].alias.toLowerCase(); // Shorthand reference to superhero's alias
 
     // Shorthand for setting image source attribute
     img.src = imgSrc;
@@ -195,6 +257,7 @@ function showName(letter) {
         lives--;
         if (lives === 0) {
             alert("Game over...you lose.");
+            identity.innerText = name;
         }
     }
     // if the nameArray does not contain dashes, increment games win by 1
@@ -202,6 +265,7 @@ function showName(letter) {
         numOfWins++;
         superhero_img.appendChild(img);
         superhero.innerText = superheroName;
+        $("#stage").modal("show");
     }
 
     // Update display with letters that were guessed correctly
@@ -211,3 +275,10 @@ function showName(letter) {
 
     return letter;
 }
+
+revealBtn.onclick = function() {
+    identity.innerHTML = superheroes[index].alias.toLowerCase();
+    superhero_img.appendChild(img);
+    superhero.innerText = superheroName;
+    $("#stage").modal("show");
+};
